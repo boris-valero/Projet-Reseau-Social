5a55147006a8dfd04c4dbe7888bd9a71f0509479
Manon Sella <manonsella@gmail.com>
2024-05-27 23:29:17 +0200

    adds php to make the news.php file dynamic

--- Niveau1/news.php
+++ Niveau1/news.php
@@ -33,28 +33,7 @@
                         tous les utilisatrices du site.</p>
                 </section>
             </aside>
-            <main>
-                <!-- L'article qui suit est un exemple pour la présentation et 
-                  @todo: doit etre retiré -->
-                <article>
-                    <h3>
-                        <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
-                    </h3>
-                    <address>par AreTirer</address>
-                    <div>
-                        <p>Ceci est un paragraphe</p>
-                        <p>Ceci est un autre paragraphe</p>
-                        <p>... de toutes manières il faut supprimer cet 
-                            article et le remplacer par des informations en 
-                            provenance de la base de donnée (voir ci-dessous)</p>
-                    </div>                                            
-                    <footer>
-                        <small>♥1012 </small>
-                        <a href="">#lorem</a>,
-                        <a href="">#piscitur</a>,
-                    </footer>
-                </article>               
-
+            <main>            
                 <?php
                 /*
                   // C'est ici que le travail PHP commence
@@ -123,13 +102,28 @@
                         <h3>
                             <time><?php echo $post['created'] ?></time>
                         </h3>
-                        <address>AREMPLACER</address>
+                        <address>
+                            <?php
+                            echo "Par " . $post['author_name'];
+                            ?>
+                        </address>
                         <div>
-                            <p>AREMPLACER</p>
+                            <p><?php
+                            echo $post['content'];
+                            ?>
+                            </p>
                         </div>
                         <footer>
-                            <small>♥ AREMPLACER </small>
-                            <a href="">AREMPLACER</a>,
+                            <small>♥<?php
+                            echo $post['like_number'];
+                            ?> </small>
+                            <?php
+                                $exploded = explode(',', $post['taglist']);
+                                foreach($exploded as $tag) {
+                                    echo '<a href="">#'.$tag.'</a>, ';
+                                }
+                            ?>
+
                         </footer>
                     </article>
                     <?php
