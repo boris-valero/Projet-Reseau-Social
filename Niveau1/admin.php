6697fe55eb70d4fe71ed977520f43e5f30ab2da7
Manon Sella <manonsella@gmail.com>
2024-05-29 16:57:00 +0200

    adding complete admin.php file

--- Niveau1/admin.php
+++ Niveau1/admin.php
@@ -92,22 +92,23 @@
                  * Etape 5 : @todo : Afficher les utilisatrices en s'inspirant de ce qui a été fait dans news.php
                  * Attention à en pas oublier de modifier dans le lien les "user_id=123" avec l'id de l'utilisatrice
                  */
-                while ($tag = $lesInformations->fetch_assoc())
+                while ($user = $lesInformations->fetch_assoc())
                 {
-                    echo "<pre>" . print_r($tag, 1) . "</pre>";
+                    echo "<pre>" . print_r($user, 1) . "</pre>";
                     ?>
                     <article>
                         <h3><?php 
-                        echo $tag["alias"];
+                        echo $user["alias"];
                         ?>
                         </h3>
-                        <p>id:123</p>
+                        <p>id:<?php echo $user["id"]; ?>
+                        </p>
                         <nav>
-                            <a href="wall.php?user_id=123">Mur</a>
-                            | <a href="feed.php?user_id=123">Flux</a>
-                            | <a href="settings.php?user_id=123">Paramètres</a>
-                            | <a href="followers.php?user_id=123">Suiveurs</a>
-                            | <a href="subscriptions.php?user_id=123">Abonnements</a>
+                            <a href="wall.php?user_id=<?php echo $user["id"]; ?>">Mur</a>
+                            | <a href="feed.php?user_id=<?php echo $user["id"]; ?>">Flux</a>
+                            | <a href="settings.php?user_id=<?php echo $user["id"]; ?>">Paramètres</a>
+                            | <a href="followers.php?user_id=<?php echo $user["id"]; ?>">Suiveurs</a>
+                            | <a href="subscriptions.php?user_id=<?php echo $user["id"]; ?>">Abonnements</a>
                         </nav>
                     </article>
                 <?php } ?>
