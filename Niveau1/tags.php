c3e9155b36be1b2aee5f03a2865a288f94d55da2
Manon Sella <manonsella@gmail.com>
2024-05-30 11:48:04 +0200

    adding completed version of tags.php

--- Niveau1/tags.php
+++ Niveau1/tags.php
@@ -58,7 +58,7 @@
                 <section>
                     <h3>Présentation</h3>
                     <p>Sur cette page vous trouverez les derniers messages comportant
-                        le mot-clé XXX
+                        le mot-clé <?php echo $tag["label"]; ?>
                         (n° <?php echo $tagId ?>)
                     </p>
 
@@ -92,7 +92,7 @@
                 }
 
                 /**
-                 * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
+                 * Etape 4: @todo Parcourir les messages et remplir correctement le HTML avec les bonnes valeurs php
                  */
                 while ($post = $lesInformations->fetch_assoc())
                 {
@@ -101,25 +101,20 @@
                     ?>                
                     <article>
                         <h3>
-                            <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
+                            <time datetime='2020-02-01 11:12:13' ><?php echo $post["created"]; ?></time>
                         </h3>
-                        <address>par AreTirer</address>
+                        <address><?php echo $post["author_name"];?></address>
                         <div>
-                            <p>Ceci est un paragraphe</p>
-                            <p>Ceci est un autre paragraphe</p>
-                            <p>... de toutes manières il faut supprimer cet 
-                                article et le remplacer par des informations en 
-                                provenance de la base de donnée</p>
+                            <p>
+                                <?php echo $post["content"];?>
+                            </p>
                         </div>                                            
                         <footer>
-                            <small>♥ 132</small>
-                            <a href="">#lorem</a>,
-                            <a href="">#piscitur</a>,
+                            <small><?php echo $post["like_number"]; ?></small>
+                            <a href="">#<?php echo $post["taglist"];?></a>,
                         </footer>
                     </article>
                 <?php } ?>
-
-
             </main>
         </div>
     </body>
