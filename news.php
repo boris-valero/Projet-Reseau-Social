115d067c79e8675ef6545d1c011fe2ec415d9541
Manon Sella <manonsella@gmail.com>
2024-06-03 16:49:19 +0200

    addinf updated version of news.php with links

--- news.php
+++ news.php
@@ -36,7 +36,6 @@
             <main>            
                 <?php
                 /*
-                  // C'est ici que le travail PHP commence
                   // Votre mission si vous l'acceptez est de chercher dans la base
                   // de données la liste des 5 derniers messsages (posts) et
                   // de l'afficher
@@ -57,19 +56,23 @@
                 }
 
                 // Etape 2: Poser une question à la base de donnée et récupérer ses informations
-                // cette requete vous est donnée, elle est complexe mais correcte, 
-                // si vous ne la comprenez pas c'est normal, passez, on y reviendra
+
                 $laQuestionEnSql = "
-                    SELECT posts.content,
-                    posts.created,
-                    users.alias as author_name,  
-                    count(likes.id) as like_number,  
-                    GROUP_CONCAT(DISTINCT tags.label) AS taglist 
+                    SELECT
+                        posts.content,
+                        posts.created,
+                        users.alias as author_name,  
+                        users.id as user_id,
+                        count(likes.id) AS like_number, 
+                        GROUP_CONCAT(DISTINCT tags.label) AS taglist 
+
                     FROM posts
-                    JOIN users ON  users.id=posts.user_id
+
+                    JOIN      users      ON users.id=posts.user_id
                     LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
                     LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
-                    LEFT JOIN likes      ON likes.post_id  = posts.id 
+                    LEFT JOIN likes      ON likes.post_id  = posts.id
+ 
                     GROUP BY posts.id
                     ORDER BY posts.created DESC  
                     LIMIT 5
@@ -84,28 +87,21 @@
                     exit();
                 }
 
-                // Etape 3: Parcourir ces données et les ranger bien comme il faut dans du html
-                // NB: à chaque tour du while, la variable post ci dessous reçois les informations du post suivant.
+                // NB: à chaque tour du while, la variable post ci dessous reçoit les informations du post suivant.
                 while ($post = $lesInformations->fetch_assoc())
                 {
-                    //la ligne ci-dessous doit etre supprimée mais regardez ce 
-                    //qu'elle affiche avant pour comprendre comment sont organisées les information dans votre 
-                    echo "<pre>" . print_r($post, 1) . "</pre>";
-
-                    // @todo : Votre mission c'est de remplacer les AREMPLACER par les bonnes valeurs
-                    // ci-dessous par les bonnes valeurs cachées dans la variable $post 
-                    // on vous met le pied à l'étrier avec created
-                    // 
-                    // avec le ? > ci-dessous on sort du mode php et on écrit du html comme on veut... mais en restant dans la boucle
+                    //echo "<pre>" . print_r($post, 1) . "</pre>";
                     ?>
                     <article>
                         <h3>
                             <time><?php echo $post['created'] ?></time>
                         </h3>
                         <address>
+                            <a href="wall.php?user_id=<?php echo $post['user_id']; ?>">
                             <?php
                             echo "Par " . $post['author_name'];
                             ?>
+                            </a>
                         </address>
                         <div>
                             <p><?php
@@ -117,20 +113,11 @@
                             <small>♥<?php
                             echo $post['like_number'];
                             ?> </small>
-                            <?php
-                                $exploded = explode(',', $post['taglist']);
-                                foreach($exploded as $tag) {
-                                    echo '<a href="">#'.$tag.'</a>, ';
-                                }
-                            ?>
+                            <a href=""><?php echo "#", $post['taglist'] ?></a>,
 
                         </footer>
                     </article>
-                    <?php
-                    // avec le <?php ci-dessus on retourne en mode php 
-                }// cette accolade ferme et termine la boucle while ouverte avant.
-                ?>
-
+                    <?php } ?>
             </main>
         </div>
     </body>
