fbfba373dacd8819cf54cfc43a6e9ffb169ea25a
Manon Sella <manonsella@gmail.com>
2024-06-03 16:52:09 +0200

    adding refactored tags.php file

--- tags.php
+++ tags.php
@@ -2,7 +2,7 @@
 <html lang="fr">
     <head>
         <meta charset="utf-8">
-        <title>ReSoC - Les message par mot-clé</title> 
+        <title>ReSoC - Les messages par mot-clé</title> 
         <meta name="author" content="Julien Falconnet">
         <link rel="stylesheet" href="style.css"/>
     </head>
@@ -22,15 +22,10 @@
                     <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
                     <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
                 </ul>
-
             </nav>
         </header>
         <div id="wrapper">
             <?php
-            /**
-             * Cette page est similaire à wall.php ou feed.php 
-             * mais elle porte sur les mots-clés (tags)
-             */
             /**
              * Etape 1: Le mur concerne un mot-clé en particulier
              */
@@ -42,7 +37,6 @@
              */
             $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
             ?>
-
             <aside>
                 <?php
                 /**
@@ -51,7 +45,6 @@
                 $laQuestionEnSql = "SELECT * FROM tags WHERE id= '$tagId' ";
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 $tag = $lesInformations->fetch_assoc();
-                //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par le label et effacer la ligne ci-dessous
                 echo "<pre>" . print_r($tag, 1) . "</pre>";
                 ?>
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
@@ -59,9 +52,8 @@
                     <h3>Présentation</h3>
                     <p>Sur cette page vous trouverez les derniers messages comportant
                         le mot-clé <?php echo $tag["label"]; ?>
-                        (n° <?php echo $tagId ?>)
+                        (n° <?php echo $tag["id"]; ?>)
                     </p>
-
                 </section>
             </aside>
             <main>
@@ -96,22 +88,25 @@
                  */
                 while ($post = $lesInformations->fetch_assoc())
                 {
-
-                    echo "<pre>" . print_r($post, 1) . "</pre>";
-                    ?>                
+                    //echo "<pre>" . print_r($post, 1) . "</pre>";
+                ?>                
                     <article>
                         <h3>
                             <time datetime='2020-02-01 11:12:13' ><?php echo $post["created"]; ?></time>
                         </h3>
-                        <address><?php echo $post["author_name"];?></address>
+                        <address>
+                            <a href="wall.php?user_id=<?php echo $post['author_name']; ?>">
+                            <?php echo $post["author_name"];?>
+                            </a>
+                        </address>
                         <div>
                             <p>
                                 <?php echo $post["content"];?>
                             </p>
                         </div>                                            
                         <footer>
-                            <small><?php echo $post["like_number"]; ?></small>
-                            <a href="">#<?php echo $post["taglist"];?></a>,
+                            <small>♥<?php echo $post["like_number"]; ?></small>
+                            <a href="">#<?php echo $post["taglist"]; ?></a>,
                         </footer>
                     </article>
                 <?php } ?>
