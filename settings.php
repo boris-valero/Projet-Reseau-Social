825dbced5987e9d4b2a3695db01d8608958cedf2
Manon Sella <manonsella@gmail.com>
2024-06-03 16:50:52 +0200

    adding updated version of settings with links

--- settings.php
+++ settings.php
@@ -34,8 +34,7 @@
                     <h3>Présentation</h3>
                     <p>Sur cette page vous trouverez les informations de l'utilisatrice
                         <?php /* " intval" convertit un string en nombre */ ?>
-                        n° <?php echo intval($_GET['user_id']) ?></p>
-
+                        n° <?php echo intval($_GET['user_id']); ?></p>
                 </section>
             </aside>
             <main>
@@ -45,7 +44,6 @@
                  * La première étape est donc de trouver quel est l'id de l'utilisatrice
                  * Celui ci est indiqué en parametre GET de la page sous la forme user_id=...
                  * Documentation : https://www.php.net/manual/fr/reserved.variables.get.php
-                 * ... mais en résumé c'est une manière de passer des informations à la page en ajoutant des choses dans l'url
                  */
                 $userId = intval($_GET['user_id']);
 
@@ -79,14 +77,14 @@
                 /**
                  * Etape 4: à vous de jouer
                  */
-                //@todo: afficher le résultat de la ligne ci dessous, remplacer les valeurs ci-après puis effacer la ligne ci-dessous
                 echo "<pre>" . print_r($user, 1) . "</pre>";
                 ?>                
                 <article class='parameters'>
                     <h3>Mes paramètres</h3>
                     <dl>
                         <dt>Pseudo</dt>
-                        <dd><?php echo $user["alias"];?></dd>
+                        <dd><?php echo $user["alias"]; ?>
+                        </dd>
                         <dt>Email</dt>
                         <dd><?php echo $user["email"];?></dd>
                         <dt>Nombre de message</dt>
