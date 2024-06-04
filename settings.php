c2d93c97b194bfde2f6b68a876d925cd52e7867e
Manon Sella <manonsella@gmail.com>
2024-06-04 16:41:05 +0200

    adding new classes to images and commenting data not to be shown

--- settings.php
+++ settings.php
@@ -8,7 +8,7 @@
     </head>
     <body>
         <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
+            <img src="profilepic.jpg" alt="Logo de notre réseau social"/>
             <nav id="menu">
                 <a href="news.php">Actualités</a>
                 <a href="wall.php?user_id=5">Mur</a>
@@ -29,7 +29,7 @@
 
 
             <aside>
-                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
+                <img src="sunnyGin.jpg" alt="Portrait de l'utilisatrice" class="user-picture"/>
                 <section>
                     <h3>Présentation</h3>
                     <p>Sur cette page vous trouverez les informations de l'utilisatrice
@@ -77,7 +77,7 @@
                 /**
                  * Etape 4: à vous de jouer
                  */
-                echo "<pre>" . print_r($user, 1) . "</pre>";
+                //echo "<pre>" . print_r($user, 1) . "</pre>";
                 ?>                
                 <article class='parameters'>
                     <h3>Mes paramètres</h3>
