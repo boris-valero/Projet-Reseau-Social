8747441ea45669774d52185466a67cbfcb90b5a4
Manon Sella <manonsella@gmail.com>
2024-06-04 16:37:40 +0200

    deleting unnecessary comments

--- include.php
+++ include.php
@@ -10,29 +10,4 @@
                     echo "</article>";
                     exit();
                 }
-                // $laQuestionEnSql = "
-                //     SELECT posts.content,
-                //     posts.created,
-                //     users.alias as author_name,  
-                //     count(likes.id) as like_number,  
-                //     GROUP_CONCAT(DISTINCT tags.label) AS taglist 
-                //     FROM posts
-                //     JOIN users ON  users.id=posts.user_id
-                //     LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
-                //     LEFT JOIN tags       ON posts_tags.tag_id  = tags.id 
-                //     LEFT JOIN likes      ON likes.post_id  = posts.id 
-                //     GROUP BY posts.id
-                //     ORDER BY posts.created DESC  
-                //     LIMIT 5
-                //     ";
-
-                //     $lesInformations = $mysqli->query($laQuestionEnSql);
-                // if ( ! $lesInformations)
-                // {
-                //     echo "<article>";
-                //     echo("Échec de la requete : " . $mysqli->error);
-                //     echo("<p>Indice: Vérifiez la requete  SQL suivante dans phpmyadmin<code>$laQuestionEnSql</code></p>");
-                //     exit();
-                // }
-                    
 ?>
